0c6c4832a8e1bcdc0ee404e6c49d9f5ef0febd46
ZZYSonny <zhaozy5520@outlook.com>
2023-10-29 14:47:07 +0000

    nvdec

--- core.py
+++ core.py
@@ -1,6 +1,7 @@
 import logging
 import torch
 import torchvision
+import torchaudio
 from torch.nn import functional as F
 from dataclasses import dataclass, asdict
 from tqdm import tqdm
@@ -34,33 +35,56 @@ class KeyConfig:
 
 
 KeyConfig1080p1x = KeyConfig(
-    200, 1000, 512, 16, 10, ContourConfig(8, 8, 2, 5, 1))
+    200, 1000, 128, 16, 10, ContourConfig(32, 32, 2, 5, 1))
 KeyConfig1080p2x = KeyConfig(
-    50, 250, 512, 16, 10, ContourConfig(8, 8, 2, 3, 2))
+    50, 250, 128, 16, 10, ContourConfig(32, 32, 2, 3, 2))
 EasyOCRArgs = dict(
     blocklist="~@#$%^&*()_+{}|:\"<>~`[]\\;'/",
     batch_size=2
 )
 
+def yuv_to_rgb(frames):
+    frames = frames.to(torch.float)
+    y = frames[0, :, :]
+    u = frames[1, :, :]
+    v = frames[2, :, :]
 
-#@torch.compile(mode="max-autotune")
-def subtitle_black_contour(rgb: torch.Tensor, config: ContourConfig):
-    channel_dim = 1
-    r = rgb.select(channel_dim, 0).unsqueeze(channel_dim)
-    grey_mask = rgb.eq(r).all(dim=channel_dim)
+    y /= 255
+    u = u / 255 - 0.5
+    v = v / 255 - 0.5
+
+    r = y + 1.14 * v
+    g = y + -0.396 * u - 0.581 * v
+    b = y + 2.029 * u
+
+    rgb = torch.stack([r, g, b], 0)
+    rgb = (rgb * 255).clamp(0, 255).to(torch.uint8)
+    return rgb
+
+def bool_to_grey(frames: torch.Tensor):
+    return frames.to(torch.uint8).mul(255)
+
+def subtitle_black_contour(yuv: torch.Tensor, config: ContourConfig):
+    y = yuv[:, 0]
+    u = yuv[:, 1]
+    v = yuv[:, 2]
+    grey_mask = torch.logical_and(
+        u==128,
+        v==128
+    )
     white_mask = torch.logical_and(
-        rgb.gt(255 - config.white).all(dim=channel_dim),
+        y>255-config.white,
         grey_mask
     )
     black_mask = torch.logical_and(
-        rgb.lt(config.black).all(dim=channel_dim),
+        y<config.black,
         grey_mask
     )
+    dtype = torch.float16 if yuv.device.type == "cuda" else torch.float32
     if config.scale == 1:
-        white_cnt_scaled = white_mask.to(torch.float16)
+        white_cnt_scaled = white_mask.to(dtype)
         black_mask_scaled = black_mask
     else:
-        dtype = torch.float16 if rgb.device.type == "cuda" else torch.float32
         white_cnt_scaled = F.avg_pool2d(
             white_mask.to(dtype),
             kernel_size=config.scale,
@@ -111,7 +135,14 @@ def subtitle_bound(frame, edge, config: KeyConfig):
 
 def key_frame_generator(path, config: KeyConfig):
     logger = logging.getLogger('KEY')
-    reader = torchvision.io.VideoReader(path, "video", num_threads=8)
+    stream = torchaudio.io.StreamReader(path)
+    stream.add_video_stream(config.batch_edge,
+                            decoder="h264_cuvid",
+                            hw_accel="cuda:0",
+                            decoder_option={
+                                "crop": "880x0x0x0"
+                            }
+                            )
 
     has_start = 0
     start_time = 0.0
@@ -123,13 +154,14 @@ def key_frame_generator(path, config: KeyConfig):
         assert (not has_start)
         has_start = True
         start_time = cur_time
-        # Clone edge so that in the next batch, previous edge_batch
-        # can be deleted.
-        start_frame = subtitle_bound(
+        # Move to CPU. start_frame will not be used for computation.
+        start_frame = yuv_to_rgb(subtitle_bound(
             subtitle_region(cur_frame),
             cur_edge,
             config
-        ).clone()
+        )).cpu()
+        # Clone edge so that in the next batch, previous edge_batch
+        # can be deleted.
         start_edge = cur_edge.clone()
 
     def release_key_frame(cur_time):
@@ -142,55 +174,32 @@ def key_frame_generator(path, config: KeyConfig):
             "frame": start_frame
         }
 
-    def stack_frames(batch):
-        return torch.stack([
-            subtitle_region(frame["data"])
-            for frame in batch
-        ]).to(config.device)
+    past_frames = 0
 
-    last_batch = []
-    while True:
-        logger.info("Decoding videos")
-        new_batch = list(itertools.islice(reader, config.batch_edge - len(last_batch)))
-        frame_batch = last_batch + new_batch
-
+    logger.info("Decoding video")
+    for (yuv_batch, ) in stream.stream():
         logger.info("Computing edges")
-        edge_batch = subtitle_black_contour(
-            stack_frames(frame_batch), config.contour)
+        edge_batch = subtitle_black_contour(yuv_batch, config.contour)
         pixels_batch = edge_batch.int().sum(dim=[1, 2])
-        empty_batch = pixels_batch.lt(config.empty).cpu()
+        empty_batch_cpu = pixels_batch.lt(config.empty).cpu()
 
-        logger.info("Batch Ready")
-        window_start = 0
-        while True:
+        window_start: int = 0
+        while window_start != yuv_batch.shape[0]:
             # Determine current window [cur_start, cur_end]
-            window_end = window_start + config.batch_window
-
-            if window_end > len(frame_batch):
-                if len(frame_batch) == config.batch_edge:
-                    logger.info(
-                        f"Window outside Batch. Leave {window_start}:{config.batch_edge} to next batch.")
-                    last_batch = frame_batch[window_start:]
-                    break
-                elif window_start == len(frame_batch):
-                    if has_start:
-                        cur_time = frame_batch[-1]["pts"]
-                        yield release_key_frame(cur_time)
-                    logger.info(f"Finished.")
-                    return
-                else:
-                    window_end = len(frame_batch)
-                    logger.info(f"Last batch and window outside batch. Do the last {window_start}:{window_end}")
-
-            loc = f"{frame_batch[window_start]['pts']:.2f} -> {frame_batch[window_end-1]['pts']:.2f}"
+            window_end: int = min(
+                window_start + config.batch_window,
+                yuv_batch.shape[0]
+            )
+            loc = f"{past_frames+window_start} -> {past_frames+window_end}"
 
             if not has_start:
                 logger.debug("="*20)
-                logger.debug(f"{loc} Pixs:\n{pixels_batch[window_start:window_end].tolist()}\n")
-                
-                non_empty_window = torch.logical_not(
-                    empty_batch[window_start:window_end])
-                i = non_empty_window.nonzero_static(
+                logger.debug(
+                    f"{loc} Pixs:\n{pixels_batch[window_start:window_end].tolist()}\n")
+
+                non_empty_window_cpu = torch.logical_not(
+                    empty_batch_cpu[window_start:window_end])
+                i = non_empty_window_cpu.nonzero_static(
                     size=1, fill_value=-1).item()
                 if i == -1:
                     window_start = window_end
@@ -198,37 +207,46 @@ def key_frame_generator(path, config: KeyConfig):
                 else:
                     window_start = window_start + i
                     logger.info(
-                        f"{loc} Empty -> Text at {frame_batch[window_start]['pts']:.2f}")
+                        f"{loc} Empty -> Text at Frame {window_start}")
                     select_key_frame(
-                        frame_batch[window_start]["pts"], frame_batch[window_start]["data"], edge_batch[window_start])
+                        past_frames+window_start, yuv_batch[window_start], edge_batch[window_start])
             else:
                 # Compute diff
                 edge_window = edge_batch[window_start:window_end]
                 diff_window = torch.logical_xor(
                     edge_window, start_edge).int().sum(dim=[1, 2])
-                changed_window = diff_window.gt(config.diff).cpu()
-                empty_window = empty_batch[window_start:window_end]
-                stop_window = torch.logical_or(changed_window, empty_window)
+                changed_window_cpu = diff_window.gt(config.diff).cpu()
+                empty_window_cpu = empty_batch_cpu[window_start:window_end]
+                stop_window_cpu = torch.logical_or(
+                    changed_window_cpu, empty_window_cpu)
 
                 logger.debug("="*20)
-                logger.debug(f"{loc} Pixs:\n{pixels_batch[window_start:window_end].tolist()}\n")
+                logger.debug(
+                    f"{loc} Pixs:\n{pixels_batch[window_start:window_end].tolist()}\n")
                 logger.debug(f"{loc} Diff:\n{diff_window.tolist()}\n")
 
-                i = stop_window.nonzero_static(size=1, fill_value=-1).item()
+                i = stop_window_cpu.nonzero_static(
+                    size=1, fill_value=-1).item()
                 if i == -1:
                     window_start = window_end
                     logger.info(f"{loc} Text:")
                 else:
                     window_start = window_start + i
-                    cur_time = frame_batch[window_start]['pts']
-                    yield release_key_frame(cur_time)
+                    yield release_key_frame(window_start)
 
-                    if empty_window[i].item():
-                        logger.info(f"{loc} Text -> Empty  at {cur_time:2f}")
+                    if empty_window_cpu[i].item():
+                        logger.info(
+                            f"{loc} Text -> Empty  at Frame {window_start}")
                     else:
-                        logger.info(f"{loc} Text -> Changed at {cur_time:2f}")
+                        logger.info(
+                            f"{loc} Text -> Changed at Frame {window_start}")
                         select_key_frame(
-                            cur_time, frame_batch[window_start]["data"], edge_batch[window_start])
+                            past_frames+window_start, yuv_batch[window_start], edge_batch[window_start])
+
+        past_frames += yuv_batch.shape[0]
+
+    if has_start:
+        yield release_key_frame(past_frames-1)
 
 
 def ocr_text_generator(key_frame_generator, easyocr_args: dict):
@@ -294,15 +312,20 @@ def srt_entry_generator(ocrs):
 
 def debug_contour(path, config: KeyConfig):
     os.system("rm debug/img/*.png")
-    reader = torchvision.io.VideoReader(path, "video")
+    stream = torchaudio.io.StreamReader(path)
+    stream.add_video_stream(1,
+                            decoder="h264_cuvid",
+                            hw_accel="cuda:0",
+                            decoder_option={
+                                "crop": "880x0x0x0"
+                            }
+                            )
 
-    for i, video_frame in enumerate(reader):
-        frame = video_frame["data"]
-        torchvision.io.write_png(frame, f"debug/img/{i}.png")
-        edge = subtitle_black_contour_single(frame, config.contour)
-        if edge.int().sum() > config.empty:
-            torchvision.io.write_png(
-                (edge*255).unsqueeze(0).to(torch.uint8), f"debug/img/{i}_.png")
+    for i, (yuv_batch,) in enumerate(stream.stream()):
+        rgb_batch = yuv_to_rgb(yuv_batch)
+        torchvision.io.write_png(rgb_batch[0].cpu(), f"debug/img/{i}.png")
+        edge = subtitle_black_contour(yuv_batch, config.contour)
+        torchvision.io.write_png(bool_to_grey(edge).cpu(), f"debug/img/{i}_.png")
 
 
 def debug_key(key_frame_generator, config: KeyConfig):
