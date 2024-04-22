5d452d3fd0e6a77d569f8a38346ac0f38b3a4d6e
ZZYSonny <zhaozy5520@outlook.com>
2024-04-22 22:57:26 +0100

    change headers

--- tools.py
+++ tools.py
@@ -1,14 +1,5 @@
-import os
-os.system("rm -rf /home/zzysonny/Documents/Code/Projects/SubtitleExtractor/debug/img/*")
-os.system("rm -rf /home/zzysonny/Documents/Code/Projects/SubtitleExtractor/debug/error/*")
 import warnings
 warnings.filterwarnings('ignore')
-#import os
-#os.environ["TORCHINDUCTOR_MAX_AUTOTUNE"]="1"
-#os.environ["TORCHINDUCTOR_MAX_AUTOTUNE_POINTWISE"]="1"
-#os.environ["TORCHINDUCTOR_LAYOUT_OPTIMIZATION"]="1"
-#os.environ["TORCHINDUCTOR_DEBUG_FUSION"]="1"
-#os.environ["TORCHINDUCTOR_PERMUTE_FUSION"]="1"
 import functools
 import os.path as osp
 import urllib.request
@@ -20,12 +11,27 @@ import socket
 import socketserver
 import requests
 import json
+import tempfile
 
-UA = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'
+HEADER = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": "\"Chromium\";v=\"123\", \"Not:A-Brand\";v=\"8\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Linux\"",
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1"
+}
 RSS_URL = "https://api.ani.rip/ani-download.xml"
-IN_VIDEO_PATH = "temp/in.mp4"
-OUT_SUBTITLE_PATH = "temp/out.srt"
-OUT_VIDEO_PATH = "temp/out.mkv"
+TEMP_DIR = tempfile.TemporaryDirectory(prefix="subs").name
+IN_VIDEO_PATH = osp.join(TEMP_DIR, "in.mp4")
+OUT_SUBTITLE_PATH = osp.join(TEMP_DIR, "in.mp4")
+OUT_VIDEO_PATH = osp.join(TEMP_DIR, "in.mp4")
 SERVE_HTTP = True
 
 RTX2060Config = SubsConfig(
@@ -73,7 +79,7 @@ config = RTX2060Config
 def get_anime_link_from_ani_xml(name: str):
     # 获取RSS
     print("下载RSS")
-    response = requests.get(f'https://api.ani.rip/ani-download.xml', header={'user-agent': UA})
+    response = requests.get(f'https://api.ani.rip/ani-download.xml', headers=HEADER)
 
     for item in reversed(ET.ElementTree(ET.fromstring(response.text)).findall("./channel/item")):
         title = item.find("title").text
@@ -86,9 +92,9 @@ def get_anime_link_from_ani_xml(name: str):
     raise Exception("未发现视频")
 
 def get_anime_link_from_ani_folder(folder: str, name:str):
-    headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36'}
     data = '{"password":"null"}'
-    response = requests.post(f'https://aniopen.an-i.workers.dev/{folder}/', headers={'user-agent': UA}, data=data)
+    print("下载File List")
+    response = requests.post(f'https://aniopen.an-i.workers.dev/{folder}/', headers=HEADER, data=data)
 
     for info in json.loads(response.text)['files']:
         if name in info["name"]:
@@ -100,7 +106,7 @@ def get_anime_link_from_ani_folder(folder: str, name:str):
     raise Exception("未发现视频")
 
 def download_anime_from_link(link: str, size: int | None = None):
-    response = requests.get(link, headers={'user-agent': UA}, stream=True)
+    response = requests.get(link, headers=HEADER, stream=True)
     pbar = tqdm(desc="下载视频", total=size*1024*1024, unit='B', unit_scale=True)
 
     with open(IN_VIDEO_PATH, 'wb') as f:
