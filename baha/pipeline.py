04c8440da90af3f7744d95dea40ed08f4bf8005e
ZZYSonny <zhaozy5520@outlook.com>
2024-04-28 16:10:39 +0100

    rename

--- baha/pipeline.py
+++ baha/pipeline.py
@@ -1,226 +1,73 @@
-import os
-import logging
-from dataclasses import dataclass
-import datetime
-
-
-from tqdm import tqdm
-import numpy as np
+import xml.etree.ElementTree as ET
 import torch
-import torchvision
-import torchaudio
-import easyocr
-import zhconv
-import srt
+import queue
+import threading
 
-from . import kernels
-from .kernels import FilterConfig
+from . import stages
 
-LOGLEVEL = os.environ.get('LOGLEVEL', 'ERROR').upper()
-logging.basicConfig(level=LOGLEVEL)
-reader = easyocr.Reader(['ch_tra'])
+config = stages.SubsConfig(
+    exe = stages.ExecConfig(
+        batch = 3,
+        device = "cuda"
+    ),
+    key = stages.KeyConfig(
+        empty=0.003, 
+        diff_tol=0.4,
+    ),
+    box = stages.CropConfig(
+        top=856,
+        down=0,
+        left=192,
+        right=192,
+        width=1920,
+        height=1080
+    ),
+    filter=stages.FilterConfig(
+        block_col = 512,
+        max_text_row = 2,
 
+        range_y_black = 24,
+        range_y_white = 56,
+        range_uv_grey = 16,
+        row_min_keep = 4,
+        col_min_keep = 4,
+        row_max_break = 16,
+        filter_white_row = 4,
+        filter_black_row = 4,
+    ),
+    ocr = dict(
+        # https://www.jaided.ai/easyocr/documentation/
+        blocklist=" `~@#$%^&*_+={}[]|\\:;<>/",
+        batch_size=16,
+        contrast_ths=0,
+        # https://github.com/clovaai/CRAFT-pytorch/issues/51
+        #text_threshold=0.3,
+        #low_text=0.2
+    )
+)
 
-@dataclass
-class CropConfig:
-    top: int
-    down: int
-    left: int
-    right: int
-    width: int
-    height: int
+def async_iterable(xs, limit=2):
+    def async_generator():
+        q = queue.Queue(limit)
 
-@dataclass
-class ExecConfig:
-    batch: int
-    device: str = "cuda"
+        def decode_worker():
+            for x in xs:
+                q.put(x)
+            q.put(None)
+            q.task_done()
+        threading.Thread(target=decode_worker, daemon=True).start()
+        while True:
+            x = q.get()
+            if x is None: break
+            else: yield x
 
-@dataclass
-class KeyConfig:
-    empty: float
-    diff_tol: float
+    if isinstance(xs, list): return xs
+    else: return async_generator()
 
-@dataclass
-class SubsConfig:
-    exe: ExecConfig
-    key: KeyConfig
-    box: CropConfig
-    filter: FilterConfig
-    ocr: dict
+def convert_subtitle(in_video_path: str, out_sub_path: str):
+    keys = async_iterable(stages.key_frame_generator(in_video_path, config))
+    ocrs = async_iterable(stages.ocr_text_generator(keys, config))
+    srts = stages.srt_generator(out_sub_path, ocrs)
+    torch.cuda.empty_cache()
 
-
-def yuv_to_rgb(frames):
-    frames = frames.to(torch.float)
-    y = frames[..., 0, :, :]
-    u = frames[..., 1, :, :]
-    v = frames[..., 2, :, :]
-
-    y /= 255
-    u = u / 255 - 0.5
-    v = v / 255 - 0.5
-
-    r = y + 1.14 * v
-    g = y + -0.396 * u - 0.581 * v
-    b = y + 2.029 * u
-
-    rgb = torch.stack([r, g, b], -3)
-    rgb = (rgb * 255).clamp(0, 255).to(torch.uint8)
-    return rgb
-
-
-def bool_to_grey(frames: torch.Tensor):
-    return frames.to(torch.uint8).mul(255)
-
-
-def key_frame_generator(in_video_path, config: SubsConfig):
-    logger = logging.getLogger('KEY')
-    stream = torchaudio.io.StreamReader(in_video_path)
-
-    info = stream.get_src_stream_info(0)
-    num_frame = stream.get_src_stream_info(0).num_frames
-    num_batch = int((num_frame + config.exe.batch - 1) // config.exe.batch)
-    assert (info.codec == "h264")
-    assert (info.format == "yuv420p")
-
-    fps = info.frame_rate
-    stream.add_video_stream(config.exe.batch,
-                            decoder="h264_cuvid",
-                            hw_accel="cuda:0",
-                            decoder_option={
-                                "crop": f"{config.box.top}x{config.box.down}x{config.box.left}x{config.box.left}x{config.box.right}"
-                            },
-                            #filter_desc=f"fps={fps}"
-                            )
-    has_start = False
-    start_time = 0.0
-    start_cnt = 0
-    start_debug = None
-    start_frame = torch.empty(0, device="cpu")
-    start_bound = torch.empty(1, device="cuda:0")
-
-    def select_key_frame(cur_time, cur_cnt, cur_frame, cur_bound):
-        nonlocal start_time, start_cnt, start_frame, start_debug, start_bound, has_start
-        has_start = True
-        start_time = cur_time
-        start_cnt = cur_cnt
-        start_bound = cur_bound
-        start_frame = kernels.filter_text_single(cur_frame, cur_bound, config.filter)
-        start_frame = kernels.filter_bounding_single(start_frame, cur_bound)
-        start_frame = start_frame.cpu().numpy()
-        if LOGLEVEL == "DEBUG": start_debug = cur_frame.cpu()
-
-    def release_key_frame(end_time):
-        nonlocal has_start, start_time
-        has_start = False
-        key = {
-            "start": datetime.timedelta(seconds=start_time),
-            "end": datetime.timedelta(seconds=end_time),
-            "frame": start_frame,
-            "debug": start_debug
-        }
-        start_time = 0.0
-        return key
-        
-    resolution_native = (config.box.width - config.box.left - config.box.right) * (config.box.height - config.box.top - config.box.down)
-    threshold_empty = int(config.key.empty * resolution_native)
-    logger.info("Decoding video")
-    for (yuv_batch, ) in tqdm(stream.stream(), total=num_batch, desc="Key", position=0):
-        pts = yuv_batch.pts
-
-        logger.info("Computing edges")
-        yuv = yuv_batch[:]
-        bound = kernels.scan_text_boundary(yuv, config.filter)
-        cnt_cpu = (bound[:,1] - bound[:,0]).clamp_min(0).sum(dim=[1, 2]).cpu().tolist()
-        diff_cpu = (bound - start_bound).abs().sum(dim=[1, 2, 3]).cpu().tolist()
-        
-        if logger.isEnabledFor(logging.DEBUG):
-            time = [
-                pts + i / fps
-                for i in range(yuv.size(0))
-            ]
-            logger.debug("="*20)
-            logger.debug("Time:%s", time)
-            logger.debug("Pixs:%s", cnt_cpu)
-            logger.debug("Diff:%s", diff_cpu)
-
-        # Assume in a batch, the state changes at most once.
-        if not has_start:
-            for i in range(yuv.size(0)):
-                if cnt_cpu[i] > threshold_empty:
-                    cur_time = pts + i / fps
-                    logger.info("Empty -> Text at %s", cur_time)
-                    select_key_frame(cur_time, cnt_cpu[i], yuv[i], bound[i])
-                    break
-        else:
-            for i in range(yuv.size(0)):
-                if cnt_cpu[i] < threshold_empty:
-                    cur_time = pts + i / fps
-                    logger.info("Text -> Empty at %s", cur_time)
-                    yield release_key_frame(cur_time)
-                    break
-                if diff_cpu[i] > config.key.diff_tol * min(start_cnt, cnt_cpu[i]):
-                    cur_time = pts + i / fps
-                    logger.info("Text -> New at %s", cur_time)
-                    yield release_key_frame(cur_time)
-                    select_key_frame(cur_time, cnt_cpu[i], yuv[i], bound[i])
-                    break
-
-    stream.remove_stream(0)
-    if start_time>0:
-        yield release_key_frame(num_frame/fps)
-
-def ocr_text_generator(key_frame_generator, config: SubsConfig):
-    logger = logging.getLogger('OCR')
-    logger.info("Loading EasyOCR Model")
-    for key in tqdm(key_frame_generator, desc="OCR", position=1):
-        if 'ocrs' in key: yield key
-        else:
-            img = np.pad(key["frame"], pad_width=16, mode='constant', constant_values=0)
-            img = key["frame"]
-            res_raw = reader.readtext(img, detail=True, paragraph=False, **config.ocr)
-            res_cht = "\n".join(p[1] for p in res_raw)
-            res_chs = zhconv.convert(res_cht, locale="zh-cn")
-            min_confidence = min((p[2] for p in res_raw), default=0)
-            
-            logger.info("%s", res_chs)
-            key["text"] = res_chs
-            key["conf"] = min_confidence
-            yield key
-
-def debug(key: dict):
-    if LOGLEVEL=="DEBUG":
-        time = str(key["start"]).replace(":", "_")
-        text = str(round(key["conf"], 2)) + "_" + key["text"]
-        torchvision.io.write_png(
-            torch.from_numpy(key["frame"]).unsqueeze(0),
-            f".debug/error/{time}_out_{text}.png"
-        )
-        if key["debug"] is not None:
-            torch.save(key["debug"], f".debug/error/{time}.pt")
-            torchvision.io.write_png(
-                yuv_to_rgb(key["debug"]),
-                f".debug/error/{time}_in_{text}.png"
-            )
-
-
-def srt_generator(out_srt_path: str, key_frame_with_text_generator):
-    entries: list[srt.Subtitle]= []
     
-    for i, key in tqdm(enumerate(key_frame_with_text_generator), desc="SRT", position=2):
-        # Debug Purpose
-        if key["conf"] < 0.05:
-            debug(key)
-        # Generate entry
-        elif len(entries)>0 and key["text"] == entries[-1].content and key["start"] - entries[-1].end < datetime.timedelta(seconds=0.1):
-            entries[-1].end = key["end"]
-            debug(key)
-        else:
-            entries.append(srt.Subtitle(
-                index = 0,
-                start = key["start"],
-                end = key["end"],
-                content = key["text"],
-            ))
-
-    with open(out_srt_path, "w", encoding="utf-8") as f:
-        f.write(srt.compose(entries))
